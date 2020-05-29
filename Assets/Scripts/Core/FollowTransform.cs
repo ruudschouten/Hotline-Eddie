61faa336b0d3946a94b4adbb4987ee7aac442f9b
ruudschouten <ruud.schouten@outlook.com>
2020-05-29 22:16:19 +0200

    Added class which will follow another transform, useful for a camera

---
+++ Assets/Scripts/Core/FollowTransform.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace Core
+{
+    public class FollowTransform : MonoBehaviour
+    {
+        [SerializeField] private Transform target;
+        [SerializeField] private Vector3 offset;
+
+        private void Update()
+        {
+            transform.position = target.position + offset;
+        }
+    }
+}
