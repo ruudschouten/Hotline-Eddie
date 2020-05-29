494c248afb773b4a37f3c664311cc772a93685b8
ruudschouten <ruud.schouten@outlook.com>
2020-05-29 22:17:50 +0200

    Enemies now walk towards the player, unless they died

--- Assets/Scripts/Characters/Character.cs
+++ Assets/Scripts/Characters/Character.cs
@@ -7,5 +7,17 @@ namespace Characters
     {
         [SerializeField] protected int maxHealth;
         [SerializeField] protected int health;
+
+        public bool IsDead => health <= 0;
+        
+        public void GetHit(int damage)
+        {
+            health -= damage;
+
+            if (IsDead)
+            {
+                Debug.Log($"I have died {transform.name}");
+            }
+        }
     }
 }
--- Assets/Scripts/Characters/Enemy.cs
+++ Assets/Scripts/Characters/Enemy.cs
@@ -1,10 +1,44 @@
-﻿
+﻿using Helpers;
 using UnityEngine;
 
 namespace Characters
 {
     public class Enemy : Character
     {
+        [SerializeField] private Texture2D deadTexture;
+        [SerializeField] private Transform target;
+        [SerializeField] private bool shouldMove;
         [SerializeField] private float movementSpeed;
+
+        private Vector3 _targetPosition;
+
+        public bool ShouldMove
+        {
+            get => shouldMove;
+            set => shouldMove = value;
+        }
+
+        private void Update()
+        {
+            if (IsDead)
+            {
+                return;
+            }
+            
+            LookAt();
+            
+            if (!shouldMove)
+            {
+                return;
+            }
+
+            transform.position -= transform.right * (movementSpeed * Time.deltaTime);
+        }
+
+        protected void LookAt()
+        {
+            var angle = LookAtHelper.GetAngleAtTarget(target.position, transform.localPosition) - 180;
+            transform.localRotation = Quaternion.Euler(0, 0, angle);
+        }
     }
 }
