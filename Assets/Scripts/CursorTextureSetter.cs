28ceaf5bc734b384119cb6c462c1a2554cdafa96
ruudschouten <ruud.schouten@outlook.com>
2020-05-29 23:28:01 +0200

    Added crosshair

---
+++ Assets/Scripts/CursorTextureSetter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+public class CursorTextureSetter : MonoBehaviour
+{
+    [SerializeField] private Texture2D texture;
+    [SerializeField] private Vector2 crosshairOffset;
+
+    private void Awake()
+    {
+        Cursor.SetCursor(texture, crosshairOffset, CursorMode.Auto);
+    }
+}
