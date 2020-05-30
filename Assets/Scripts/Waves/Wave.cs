1f67a03e34b1947bf1726491db3c8f59b9970c40
ruudschouten <ruud.schouten@outlook.com>
2020-05-30 17:25:26 +0200

    Reworked Waves so they are now managed by WaveManager. Waves can now also start after time passes, instead of only starting when all enemies are killed

---
+++ Assets/Scripts/Waves/Wave.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using Characters;
+using NaughtyAttributes;
+using UnityEngine;
+using Waves;
+using Random = UnityEngine.Random;
+
+public class Wave : MonoBehaviour
+{
+    [SerializeField] private EnemyAmountDictionary enemies;
+    [SerializeField] private ConsumableAmountDictionary consumables;
+    [SerializeField] private AudioClip waveMusic;
+    [SerializeField] private bool hasNextWave;
+
+    [SerializeField] [ShowIf("hasNextWave")]
+    private Wave nextWave;
+
+    [SerializeField] [ShowIf("hasNextWave")]
+    private float secondsToWaitBeforeStartingNextWave;
+
+    public bool Started => _started;
+
+    public Player Player
+    {
+        get => _player;
+        set => _player = value;
+    }
+
+    private Player _player;
+    private Rect _areaSize;
+    private Transform[] _enemySpawnLocations;
+    private AudioSource _musicPlayer;
+
+    private WaveManager _manager;
+
+    private bool _started;
+    private int _defeatedEnemies;
+
+    public void Initialize(Player player, Rect areaSize, Transform[] spawnLocations, AudioSource musicPlayer,
+        WaveManager manager)
+    {
+        _player = player;
+        _areaSize = areaSize;
+        _enemySpawnLocations = spawnLocations;
+        _musicPlayer = musicPlayer;
+
+        _manager = manager;
+    }
+
+    public void EnemyKilled()
+    {
+        _defeatedEnemies++;
+
+        if (_defeatedEnemies >= enemies.Keys.Count)
+        {
+            if (hasNextWave)
+            {
+                if (nextWave.Started)
+                {
+                    return;
+                }
+
+                // If all enemies are killed, and a next wave is available, start the next wave.
+                _manager.StartNextWave(true);
+            }
+            else
+            {
+                // TODO: Player won the game, show screen.
+            }
+        }
+    }
+
+    private void StartNextWaveAfter(float seconds)
+    {
+        StartCoroutine(StartNextWaveRoutine(seconds));
+    }
+
+    private IEnumerator StartNextWaveRoutine(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        if (nextWave.Started)
+        {
+            yield break;
+        }
+
+        _manager.StartNextWave(true);
+    }
+
+    public void Spawn(bool playMusic)
+    {
+        _started = true;
+
+        if (playMusic)
+        {
+            StartWaveMusic();
+        }
+
+        SpawnEnemies();
+        SpawnConsumables();
+
+        if (hasNextWave)
+        {
+            StartNextWaveAfter(secondsToWaitBeforeStartingNextWave);
+        }
+    }
+
+    private void SpawnEnemies()
+    {
+        foreach (var pair in enemies)
+        {
+            for (var i = 0; i < pair.Value; i++)
+            {
+                var enemy = Instantiate(pair.Key, GetRandomPosition(), Quaternion.identity);
+                enemy.Initialize(_player);
+                enemy.OnDeathEvent.AddListener(EnemyKilled);
+            }
+        }
+    }
+
+    private void SpawnConsumables()
+    {
+        foreach (var pair in consumables)
+        {
+            for (var i = 0; i < pair.Value; i++)
+            {
+                var consumable = Instantiate(pair.Key);
+                consumable.transform.localPosition = GetRandomPositionOnArea();
+                consumable.Initialize(_player);
+            }
+        }
+    }
+
+    public void StartWaveMusic()
+    {
+        if (_musicPlayer.clip != waveMusic)
+        {
+            _musicPlayer.Stop();
+            _musicPlayer.clip = waveMusic;
+            _musicPlayer.Play();
+        }
+    }
+
+    private Vector2 GetRandomPositionOnArea()
+    {
+        var x = Random.Range(_areaSize.x, _areaSize.y);
+        var y = Random.Range(_areaSize.width, _areaSize.height);
+        return new Vector2(x, y);
+    }
+
+    private Vector3 GetRandomPosition()
+    {
+        return _enemySpawnLocations[Random.Range(0, _enemySpawnLocations.Length)].position;
+    }
+}
---
+++ Assets/Scripts/Waves/WaveManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using Characters;
+using UnityEngine;
+
+namespace Waves
+{
+    public class WaveManager : MonoBehaviour
+    {
+        [SerializeField] private Wave[] waves;
+        [SerializeField] private Transform[] enemySpawnLocations;
+        [Space]
+        [SerializeField] private Player player;
+        [SerializeField] private Rect areaSize;
+        [SerializeField] private AudioSource musicPlayer;
+
+        private int _currentWave;
+
+        public void StartDelayed(float seconds)
+        {
+            StartCoroutine(SpawnRoutine(seconds));
+        }
+        
+        private IEnumerator SpawnRoutine(float seconds)
+        {
+            waves[_currentWave].Initialize(player, areaSize, enemySpawnLocations, musicPlayer, this);
+            yield return new WaitForEndOfFrame();
+            waves[_currentWave].StartWaveMusic();
+            yield return new WaitForSeconds(seconds);
+            waves[_currentWave].Spawn(false);
+        }
+        
+        public void StartNextWave(bool playMusic)
+        {
+            waves[_currentWave].enabled = false;
+            _currentWave++;
+            waves[_currentWave].Initialize(player, areaSize, enemySpawnLocations, musicPlayer, this);
+            waves[_currentWave].Spawn(playMusic);
+        }
+    }
+}
