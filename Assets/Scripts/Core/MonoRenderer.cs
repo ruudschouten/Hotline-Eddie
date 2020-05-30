9031a78ea2557eac2d758737d5837c42145a38b5
ruudschouten <ruud.schouten@outlook.com>
2020-05-30 16:55:15 +0200

    Added lake

--- Assets/Scripts/Core/MonoRenderer.cs
+++ Assets/Scripts/Core/MonoRenderer.cs
@@ -3,13 +3,13 @@
 namespace Core
 {
     [RequireComponent(typeof(Rigidbody2D))]
-    [RequireComponent(typeof(BoxCollider2D))]
+    [RequireComponent(typeof(Collider2D))]
     [RequireComponent(typeof(SpriteRenderer))]
     public class MonoRenderer : MonoBehaviour
     {
         [SerializeField] protected new Rigidbody2D rigidbody;
         [SerializeField] protected new SpriteRenderer renderer;
-        [SerializeField] protected new BoxCollider2D collider;
+        [SerializeField] protected new Collider2D collider;
 
         public Rigidbody2D Rigidbody
         {
@@ -17,7 +17,7 @@ namespace Core
             set => rigidbody = value;
         }
 
-        public BoxCollider2D Collider
+        public Collider2D Collider
         {
             get => collider;
             set => collider = value;
