0b63c3442d6b17e7010056876e15e9e3aee12d82
ruudschouten <ruud.schouten@outlook.com>
2020-05-30 11:48:58 +0200

    CursorSetter now reapplies the cursor when it loses focus

--- Assets/Scripts/CursorTextureSetter.cs
+++ Assets/Scripts/CursorTextureSetter.cs
@@ -6,7 +6,28 @@ public class CursorTextureSetter : MonoBehaviour
     [SerializeField] private Texture2D texture;
     [SerializeField] private Vector2 crosshairOffset;
 
+    private bool _resetTexture;
+
     private void Awake()
+    {
+        SetCursor();
+    }
+
+    private void Update()
+    {
+        if (_resetTexture)
+        {
+            SetCursor();
+            _resetTexture = false;
+        }
+    }
+
+    private void OnApplicationFocus(bool hasFocus)
+    {
+        _resetTexture = !hasFocus;
+    }
+
+    private void SetCursor()
     {
         Cursor.SetCursor(texture, crosshairOffset, CursorMode.Auto);
     }
