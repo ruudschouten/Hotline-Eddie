4230571aa52a9e80acdec7ec7ab05d8662418bd9
ruudschouten <ruud.schouten@outlook.com>
2020-05-31 18:15:07 +0200

    Moved all range related stuff in Boss to MOONMOON

--- Assets/Scripts/Characters/Boss.cs
+++ Assets/Scripts/Characters/Boss.cs
@@ -8,22 +8,12 @@ namespace Characters
         [SerializeField] protected AudioClip halfHealthClip;
         [SerializeField] protected AudioClip threeQuartersHealthClip;
         [SerializeField] protected AudioClip killClip;
-        [Space] [SerializeField] protected Bullet bulletPrefab;
-        [SerializeField] protected Transform bulletEmitTransform;
-        [SerializeField] protected float minRangedDistance;
-        [SerializeField] protected float maxRangedDistance;
-        [SerializeField] protected float rangedCooldown;
-        [SerializeField] protected UnityEvent halfASecondBeforeShootEvent;
-        [SerializeField] protected UnityEvent onShootEvent;
 
         protected float Distance;
 
         protected bool IsMeleeRecharging;
         private float _meleeCooldownTimer;
 
-        // We set this to true from the start, so the boss won't shoot as soon as the player sees him.
-        protected bool IsRangeRecharging = true;
-        private float _rangeCooldownTimer;
 
         private bool _playedHalfClip;
         private bool _playedThreeQuartersClip;
@@ -90,21 +80,8 @@ namespace Characters
             DetermineAction();
         }
 
-        private void UpdateTimers()
+        protected virtual void UpdateTimers()
         {
-            if (IsRangeRecharging)
-            {
-                IsRangeRecharging = !AdvanceAndCheckTimer(ref _rangeCooldownTimer, rangedCooldown);
-                if (ShouldInvokeRangeTell)
-                {
-                    if (_rangeCooldownTimer <= 0.5f)
-                    {
-                        halfASecondBeforeShootEvent.Invoke();
-                        ShouldInvokeRangeTell = false;
-                    }
-                }
-            }
-
             if (IsMeleeRecharging)
             {
                 IsMeleeRecharging = !AdvanceAndCheckTimer(ref _meleeCooldownTimer, timeBetweenAttacks);
@@ -114,52 +91,27 @@ namespace Characters
         protected virtual void DetermineAction()
         {
             CalculateDistance();
-           CheckForShooting();
-           CheckForMelee();
+            CheckForMelee();
         }
 
         protected virtual void CalculateDistance()
         {
-            
             // Get the distance between the player and the boss.
             Distance = Vector3.Distance(transform.position, player.transform.position);
         }
 
-        protected virtual void CheckForShooting()
-        {
-            // Check if player is outside of melee distance.
-            if (Distance >= meleeDistance)
-            {
-                // Check if the ranged attack is ready.
-                if (!IsRangeRecharging)
-                {
-                    // Check if the player is between the min and max range distances
-                    if (Distance >= minRangedDistance && Distance <= maxRangedDistance)
-                    {
-                        Shoot();
-
-                        IsRangeRecharging = true;
-                    }
-                    else
-                    {
-                        Move();
-                    }
-                }
-                else
-                {
-                    // If the player is still recharging, move towards the player, hoping to get into melee range.
-                    Move();
-                }
-            }
-        }
-
         protected virtual void CheckForMelee()
         {
-            if (!IsMeleeRecharging)
+            if (!(Distance <= meleeDistance))
             {
-                MeleeAttack();
-                IsMeleeRecharging = true;
+                return;
             }
+            if (IsMeleeRecharging)
+            {
+                return;
+            }
+            MeleeAttack();
+            IsMeleeRecharging = true;
         }
 
         public virtual void MeleeAttack()
@@ -167,11 +119,6 @@ namespace Characters
             _meleeCooldownTimer = 0;
             onHitTarget.Invoke();
             player.GetHit(meleeDamage);
-        }
-
-        public virtual void Shoot()
-        {
-            _rangeCooldownTimer = 0;
         }
     }
 }
--- Assets/Scripts/Characters/MOONMOON.cs
+++ Assets/Scripts/Characters/MOONMOON.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Characters
 {
@@ -8,11 +9,77 @@ namespace Characters
         [SerializeField] protected float bulletPlacementRandomization;
         [SerializeField] protected int bulletsToFireAtOnce;
         [SerializeField] protected float secondsBetweenShots;
+        [Space] [SerializeField] protected Bullet bulletPrefab;
+        [SerializeField] protected Transform bulletEmitTransform;
+        [SerializeField] protected float minRangedDistance;
+        [SerializeField] protected float maxRangedDistance;
+        [SerializeField] protected float rangedCooldown;
+        [SerializeField] protected UnityEvent halfASecondBeforeShootEvent;
+        [SerializeField] protected UnityEvent onShootEvent;
+        
+        // We set this to true from the start, so the boss won't shoot as soon as the player sees him.
+        private bool _isRangeRecharging = true;
+        private float _rangeCooldownTimer;
 
-        public override void Shoot()
+        private void Shoot()
         {
-            base.Shoot();
+            _rangeCooldownTimer = 0;
             StartCoroutine(ShootRoutine());
+        }
+
+        protected override void UpdateTimers()
+        {
+            base.UpdateTimers();
+            
+            if (_isRangeRecharging)
+            {
+                _isRangeRecharging = !AdvanceAndCheckTimer(ref _rangeCooldownTimer, rangedCooldown);
+                if (ShouldInvokeRangeTell)
+                {
+                    if (_rangeCooldownTimer <= 0.5f)
+                    {
+                        halfASecondBeforeShootEvent.Invoke();
+                        ShouldInvokeRangeTell = false;
+                    }
+                }
+            }
+        }
+
+        protected override void DetermineAction()
+        {
+            CalculateDistance();
+            CheckForShooting();
+            CheckForMelee();
+        }
+
+        protected virtual void CheckForShooting()
+        {
+            // Check if player is outside of melee distance.
+            if (!(Distance >= meleeDistance))
+            {
+                return;
+            }
+            
+            // Check if the ranged attack is ready.
+            if (!_isRangeRecharging)
+            {
+                // Check if the player is between the min and max range distances
+                if (Distance >= minRangedDistance && Distance <= maxRangedDistance)
+                {
+                    Shoot();
+
+                    _isRangeRecharging = true;
+                }
+                else
+                {
+                    Move();
+                }
+            }
+            else
+            {
+                // If the player is still recharging, move towards the player, hoping to get into melee range.
+                Move();
+            }
         }
 
         private IEnumerator ShootRoutine()
@@ -35,7 +102,7 @@ namespace Characters
             }
 
             ShouldInvokeRangeTell = true;
-            IsRangeRecharging = true;
+            _isRangeRecharging = true;
         }
     }
 }
