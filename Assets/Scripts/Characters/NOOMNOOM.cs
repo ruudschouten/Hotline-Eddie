b01a3e9da8b2c41872f9801271b3999b9aeed74f
ruudschouten <ruud.schouten@outlook.com>
2020-05-31 16:50:59 +0200

    Added NOOMNOOM class

---
+++ Assets/Scripts/Characters/NOOMNOOM.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Characters
+{
+    public class NOOMNOOM : Boss
+    {
+        public override void MeleeAttack()
+        {
+            base.MeleeAttack();
+        }
+
+        public override void Shoot()
+        {
+            base.Shoot();
+        }
+    }
+}
