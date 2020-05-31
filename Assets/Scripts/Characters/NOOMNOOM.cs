c8dc55ee9400d65c73a7dd739578a0852339aac9
ruudschouten <ruud.schouten@outlook.com>
2020-05-31 19:17:53 +0200

    NOOMNOOM will now pull the player towards him

--- Assets/Scripts/Characters/NOOMNOOM.cs
+++ Assets/Scripts/Characters/NOOMNOOM.cs
@@ -4,14 +4,65 @@ namespace Characters
 {
     public class NOOMNOOM : Boss
     {
-        public override void MeleeAttack()
+        [SerializeField] private float pullPower;
+        [SerializeField] private float pullDuration;
+        [SerializeField] private float pullCooldown;
+
+        private float _pullActiveTimer;
+        private bool _isPulling;
+        private float _pullCooldownTimer;
+        private bool _isPullRecharging = true;
+
+        protected override void UpdateTimers()
         {
-            base.MeleeAttack();
+            base.UpdateTimers();
+
+            if (_isPullRecharging)
+            {
+                _isPullRecharging = !AdvanceAndCheckTimer(ref _pullCooldownTimer, pullCooldown);
+            }
+            else
+            {
+                if (_isPulling)
+                {
+                    _pullActiveTimer += Time.deltaTime;
+                    if (_pullActiveTimer >= pullDuration)
+                    {
+                        _isPulling = false;
+                        _isPullRecharging = true;
+                        _pullCooldownTimer = 0;
+                    }
+                }
+            }
         }
 
-        public override void Shoot()
+        protected override void DetermineAction()
         {
-            base.Shoot();
+            _isPulling = !_isPullRecharging;
+            if (!_isPulling)
+            {
+                _pullActiveTimer = 0;
+            }
+            else
+            {
+                PullPlayer();
+            }
+
+            CalculateDistance();
+            if (ShouldMelee())
+            {
+                return;
+            }
+            if (!shouldMove)
+            {
+                return;
+            }
+            Move();
+        }
+
+        private void PullPlayer()
+        {
+            player.transform.position += transform.right * (pullPower * Time.deltaTime);
         }
     }
 }
