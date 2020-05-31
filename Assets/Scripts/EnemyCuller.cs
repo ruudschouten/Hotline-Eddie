9b24e72d425ab3e60d4783329ef6415ac3e4a2af
ruudschouten <ruud.schouten@outlook.com>
2020-05-31 10:57:17 +0200

    Added Enemy Culler which will kill all enemies inside waves

---
+++ Assets/Scripts/EnemyCuller.cs
@@ -0,0 +1,22 @@
+﻿using Characters;
+using UnityEngine;
+
+public class EnemyCuller : MonoBehaviour
+{
+    [SerializeField] private Transform waveManager;
+    
+    public void CullEnemies()
+    {
+        foreach (Transform wave in waveManager)
+        {
+            foreach (Transform child in wave)
+            {
+                if (child.CompareTag("Enemy"))
+                {
+                    var enemy = GetComponent<Enemy>();
+                    enemy.GetHit(9999999);
+                }
+            }
+        }
+    }
+}
