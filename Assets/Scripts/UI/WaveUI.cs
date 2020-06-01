9f0071ee819785b2731c5b0362e717b916295811
ruudschouten <ruud.schouten@outlook.com>
2020-06-01 12:03:38 +0200

    Added a lot more waves The UI now also displays what Wave you're in

--- Assets/Scripts/UI/WaveUI.cs
+++ Assets/Scripts/UI/WaveUI.cs
@@ -1,7 +1,20 @@
-﻿namespace UI
+﻿using TMPro;
+using UnityEngine;
+
+namespace UI
 {
-    public class WaveUI
+    public class WaveUI : MonoBehaviour
     {
-        
+        [SerializeField] private TMP_Text messageText;
+
+        public void SetWaveNumber(int number)
+        {
+            messageText.text = $"Wave {number:00}";
+        }
+
+        public void SetText(string text)
+        {
+            messageText.text = text;
+        }
     }
 }
