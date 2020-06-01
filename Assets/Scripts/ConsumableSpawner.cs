6eddd8bc98d6f66a115cec02395edad819426848
ruudschouten <ruud.schouten@outlook.com>
2020-06-01 12:59:47 +0200

    Made a script which spawns a consumable

---
+++ Assets/Scripts/ConsumableSpawner.cs
@@ -0,0 +1,17 @@
+﻿using Characters;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class ConsumableSpawner : MonoBehaviour
+    {
+        [SerializeField] private Consumable consumable;
+        [SerializeField] private Player player;
+
+        public void Spawn()
+        {
+            var spawn = Instantiate(consumable, Vector3.zero, Quaternion.identity);
+            spawn.Initialize(player);
+        }
+    }
+}
