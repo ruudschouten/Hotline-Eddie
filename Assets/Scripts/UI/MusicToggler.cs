d915a732ed9bb92e1e43f5fecc317e0bcc6706db
ruudschouten <ruud.schouten@outlook.com>
2020-06-01 12:42:09 +0200

    Added script which can mute the music

---
+++ Assets/Scripts/UI/MusicToggler.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+namespace UI
+{
+    public class MusicToggler : MonoBehaviour
+    {
+        [SerializeField] private AudioMixerSnapshot muted;
+        [SerializeField] private AudioMixerSnapshot unmuted;
+
+        private bool notMuted = true;
+
+        public void Toggle()
+        {
+            if (notMuted)
+            {
+                muted.TransitionTo(0f);
+            }
+            else
+            {
+                unmuted.TransitionTo(0f);
+            }
+
+            notMuted = !notMuted;
+        }
+    }
+}
